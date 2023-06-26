23e2e88c00d302f902b9ec6cd44f06b69d97fb07
tales furlan <tales.furlan@gmail.com>
2023-06-26 11:04:52 -0300

    all questions comments been displayed

--- resources/views/pdf/heuristics.blade.php
+++ resources/views/pdf/heuristics.blade.php
@@ -92,146 +92,20 @@ h1{
 </head>
 <body>
 <?php
-$example = '[
-    {
-        "total": 6,
-        "lastUpdate": null,
-        "progress": "100.0",
-        "submitted": true,
-        "userDocId": "FYRP8ZTFYYdqY1zkNUOu9kPxGKb2",
-        "heuristicQuestions": [
-            {
-                "heuristicTotal": 5,
-                "heuristicId": 1,
-                "heuristicTitle": "Visibilidad y estado del sistema ",
-                "heuristicQuestions": [
-                    {
-                        "heuristicAnswer": 1,
-                        "heuristicComment": "",
-                        "heuristicId": "1"
-                    },
-                    {
-                        "heuristicId": "2",
-                        "heuristicAnswer": 1,
-                        "heuristicComment": ""
-                    },
-                    {
-                        "heuristicAnswer": 1,
-                        "heuristicId": "3",
-                        "heuristicComment": ""
-                    },
-                    {
-                        "heuristicAnswer": 1,
-                        "heuristicId": "4",
-                        "heuristicComment": ""
-                    },
-                    {
-                        "heuristicAnswer": 0,
-                        "heuristicId": "5",
-                        "heuristicComment": ""
-                    }
-                ]
-            },
-            {
-                "heuristicId": 2,
-                "heuristicTitle": "bla",
-                "heuristicQuestions": [
-                    {
-                        "heuristicId": "1",
-                        "heuristicAnswer": 0.5,
-                        "heuristicComment": ""
-                    }
-                ],
-                "heuristicTotal": 1
-            }
-        ]
-    },
-    {
-        "progress": "100.0",
-        "userDocId": "VCbA9EMKkpOhVZlXFfrOu812CPq2",
-        "lastUpdate": null,
-        "heuristicQuestions": [
-            {
-                "heuristicId": 1,
-                "heuristicQuestions": [
-                    {
-                        "heuristicAnswer": 1,
-                        "heuristicComment": "Yes, the Google website includes visible titles for its pages,.",
-                        "heuristicId": "1"
-                    },
-                    {
-                        "heuristicComment": " sections of the website as needed.",
-                        "heuristicAnswer": 0,
-                        "heuristicId": "2"
-                    },
-                    {
-                        "heuristicAnswer": 0.5,
-                        "heuristicComment": "Yes, it is essential for a system or application to provide clear feedback and indications to users about what actions or processes are taking place. Users should have a clear understanding of what the system is doing at any given moment.\n\nTo ensure users know what the system or application is doing, consider the following practices:\n\nProvide visual cues: Use loading indicators, progress bars, or status messages to indicate ongoing processes or actions. This helps users understand that the system is working on their request.",
-                        "heuristicId": "3"
-                    },
-                    {
-                        "heuristicComment": "When the links are not clearly defined, it means that there is a lack of clarity or specificity in how the links are presented or labeled on a website or application. This can have a negative impact on the user experience and make it difficult for users to understand the purpose or destination of the links.",
-                        "heuristicId": "4",
-                        "heuristicAnswer": 0
-                    },
-                    {
-                        "heuristicAnswer": 1,
-                        "heuristicComment": "\nIn general, it is ideal for all actions within a system or application to be directly visible to users without requiring additional actions. This approach improves usability and ensures a smooth user experience. When actions are directly visible, users can easily identify and access the available options, reducing cognitive load and saving time.",
-                        "heuristicId": "5"
-                    }
-                ],
-                "heuristicTotal": 5,
-                "heuristicTitle": "Visibilidad y estado del sistema "
-            },
-            {
-                "heuristicTotal": 1,
-                "heuristicQuestions": [
-                    {
-                        "heuristicId": "1",
-                        "heuristicAnswer": 0,
-                        "heuristicComment": "não entendi a questão"
-                    }
-                ],
-                "heuristicId": 2,
-                "heuristicTitle": "bla"
-            }
-        ],
-        "submitted": true,
-        "total": 6
-    }
-]';$options = '[
-    {
-        "description": "you agree",
-        "value": 1,
-        "text": "Si"
-    },
-    {
-        "description": "you dont totally agree",
-        "text": "no si/ no no",
-        "value": 0.5
-    },
-    {
-        "description": "you disagree",
-        "text": "no",
-        "value": 0
-    },
-    {
-        "text": "no se aplica",
-        "description": "a questão não se aplica",
-        "value": null
-    }
-]';
 
 // Decode the JSON data
-$exampleArray = $data['testAnswers'];
+$Answers = $data['testAnswers'];
 $optionsArray = $data['testOptions'];
+
 
 
 // Counter array to store the element count
 $elementCount = array();
+// Comments array to store the answer comments
+$commentsArray = array();
 
 // Iterate through each item in the example data
-foreach ($exampleArray as $item) {
+foreach ($Answers as $item) {
     // Iterate through each heuristic in the item
     foreach ($item['heuristicQuestions'] as $index => $heuristic) {
         // Iterate through each question in the heuristic
@@ -239,7 +113,14 @@ foreach ($exampleArray as $item) {
             // Get the question ID and answer
             $questionId = $question['heuristicId'];
             $answer = $question['heuristicAnswer'];
-            
+
+            $comment = $question['heuristicComment'];
+
+            // Store the comment in the comments array
+            if (!isset($commentsArray[$index][$questionId])) {
+                $commentsArray[$index][$questionId] = array();
+            }
+            $commentsArray[$index][$questionId][] = $comment;
             // Find the corresponding text in the options array
             $text = null;
             foreach ($optionsArray as $option) {
@@ -255,10 +136,21 @@ foreach ($exampleArray as $item) {
             } else {
                 $elementCount[$index][$questionId][$text]++;
             }
+
         }
     }
 }
 
+
+foreach ($commentsArray as $index => $heuristics) {
+    echo "<h2>Heuristic " . ($index + 1) . "</h2>";
+    foreach ($heuristics as $questionId => $comments) {
+        echo "<h3>Question ID: " . $questionId . "</h3>";
+        foreach ($comments as $comment) {
+            echo "<p>" . $comment . "</p>";
+        }
+    }
+}
 ?>
 
 <?php foreach ($elementCount as $index => $questions) : ?>
@@ -282,74 +174,49 @@ foreach ($exampleArray as $item) {
     <?php endforeach; ?>
 
     <div class="heuristic">
-        <h1>Reasearch Heuristics</h1>
-        @foreach($data['heuristics'] as $key => $item)
-            <div class="question">
-                <h3 class="question-title">Heuristic question {{ $key + 1 }}: {{ $item['title'] }}</h3>
-                <p>Heuristic extensive description for human eyes.</p>
-            </div>
-            <div class="chart">
-                <?php
+    <h1>Research Heuristics</h1>
+@foreach($data['heuristics'] as $key => $item)
+    <div class="question">
+        <h3 class="question-title">Heuristic question {{ $key + 1 }}: {{ $item['title'] }}</h3>
+        <p>Heuristic extensive description for human eyes.</p>
+    </div>
 
-                    foreach ($data['testOptions'] as $option) {
-                        if (isset($option['text'])) {
-                            $variableName = $option['text'];
-                            $auxOptions[$variableName] = 0; // Initialize variable with value 0
-                        }
-                        if (isset($option['value'])) {
-                            $auxOptionsValue[] = $option['value'];
-                        }
-                    }
+    <div class="chart">
+        <?php
+        $heuristicQuestions = $data['heuristics'][$key]['questions'];
 
-                    foreach ($data['testAnswers'] as $index => $answer) {
-                        foreach ($answer['heuristicQuestions'] as $heuristics) {
-                            
-                            foreach ($heuristics['heuristicQuestions'] as $questions) {
-                                $heuristicAnswer = $questions['heuristicAnswer'];
-                                foreach ($data['testOptions'] as $option) {
-                                    if (isset($option['value']) && $option['value'] == $heuristicAnswer) {
-                                        $variableName = $option['text'];
-                                        if (isset($auxOptions[$variableName])) {
-                                            $auxOptions[$variableName]++; // Increment the respective variable
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-    
+        foreach ($heuristicQuestions as $question) {
+            $questionId = $question['id'];
+            $total = array_sum($elementCount[$key][$questionId]);
 
-                    $total = array_sum($auxOptions);
+            $colors = ['#EE303A', '#F53D3B', '#FD533A', '#FE5B38', '#FF6937', '#FF7B2F', '#FF8D23']; // Orange tones
+            echo '<h4>'. $question['title']. '</h4>';
+            echo '<p>'. $question['descriptions']. '</p>';
+            foreach ($elementCount[$key][$questionId] as $text => $value) {
+                $percentage = ($value / $total) * 100;
+                $width = round($percentage, 2);
 
-                    $colors = ['#EE303A', '#F53D3B', '#FD533A','#FE5B38', '#FF6937', '#FF7B2F', '#FF8D23']; // Orange tones
-        
-                    foreach ($auxOptions as $index => $value) {
-                        $percentage = ($value / $total) * 100;
-                        $width = round($percentage, 2);
+                $colorIndex = round(($value / $total) * (count($colors) - 1));
+                $color = $colors[$colorIndex];
 
-                        $colorIndex = round(($value / $total) * (count($colors) - 1));
-                        $color = $colors[$colorIndex];
-
-                        echo '<div class="bar" style="width: ' . $width . '%; background-color: ' . $color . ';">';
-                        echo '<div class="bar-value">' . $value . '</div>';
-                        echo '</div>'; //end of the chart section
-                    }
-
-            ?>
-                    @foreach ($item['questions'] as $question)
-                        <li class="answers">
-                            <h4>{{ $question['title'] }}</h4>
-                            <p>{{ $question['descriptions'] }}</p>
-                            <ul>
-                              
-                            </ul>
-                            
-                        </li>
-                        <div class="page-break"></div>
-                    @endforeach
-
-           
-        @endforeach
+                echo '<div class="bar" style="width: ' . $width . '%; background-color: ' . $color . ';">';
+                echo '<div class="bar-value">' . $value . '</div>';
+                echo '</div>'; //end of the bar section
+            }
+            
+            // Display the corresponding comments for the current chart
+            if (isset($commentsArray[$key][$questionId])) {
+                echo '<ul>';
+                foreach ($commentsArray[$key][$questionId] as $comment) {
+                    echo '<li>' . $comment . '</li>';
+                }
+                echo '</ul>';
+            }
+        }
+        ?>
+        <div class="page-break"></div>
+    </div>
+@endforeach
     </div>
 </body>
 </html>

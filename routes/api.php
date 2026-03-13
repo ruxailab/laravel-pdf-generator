03d337c4da8940a9045ce96eeda485a926302a3f
jvJUCA <jucafranzoni1@gmail.com>
2026-03-13 09:53:51 -0300

    fix: broken error defining header

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,10 @@
-﻿<?php
+<?php
 
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
-
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type");
 
 Route::post('/generate-pdf', function (Request $request) {
     ini_set('max_execution_time', 600);
@@ -77,7 +74,9 @@ Route::post('/generate-pdf', function (Request $request) {
 
         return response($pdfStream, 200)
             ->header('Content-Type', 'application/pdf')
-            ->header('Content-Disposition', "attachment; filename=\"{$filename}\"");
+            ->header('Content-Disposition', "attachment; filename=\"{$filename}\"")
+            ->header('Access-Control-Allow-Methods', 'POST')
+            ->header('Access-Control-Allow-Headers', 'Content-Type');
     } catch (\Throwable $e) {
         $errorId = (string) Str::uuid();
 
